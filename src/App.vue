8d4dd848d1750efb998a158492e02173b035df03
Lukasz <lzahuta@divante.pl>
2020-03-02 16:22:17 +0100

    add styles, add adding items to list functionality

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title main-title">Vue Todo</h1>
+    <ToDo msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDo from "./components/ToDo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    ToDo
   }
-}
+};
 </script>
 
 <style>
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.main-title {
+  font-size: 24px;
+  font-weight: bold;
 }
 </style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="todo">
+    <section class="hello flex-item">
+      <h2 class="title">Hi {{ userName }}</h2>
+      <div v-if="!userName">
+        <p class="para">what's your name?</p>
+        <input v-model.lazy="userName" />
+      </div>
+    </section>
+
+    <section class="todo-list flex-item">
+      <div class="list-add flex-item">
+        <h2 class="title">Add to your list:</h2>
+        <input placeholder="New item" v-model="todoItem"/>
+        <button class="btn-add" @click="addItem(todoItem)">Add</button>
+      </div>
+      <div class="list-list flex-item">
+        <h2 class="title">Your list:</h2>
+        <ul class="list">
+          <li class="list-item" v-for="item in todoList" :key="item">
+            {{item}}
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDo",
+  props: {
+    msg: String
+  },
+  methods: {
+    addItem: function(value) {
+      console.log("pingu", this);
+      console.log(this.todoList);
+      this.todoList.push(value);
+    }
+  },
+  data: function() {
+    return {
+      userName: false,
+      todoItem: "",
+      todoList: ['zrobic paczingsun', 'uakame']
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.todo {
+  display: flex;
+  flex-direction: column;
+}
+.title {
+  font-size: 18px;
+  font-weight: bold;
+}
+.todo-list {
+  margin-top: 40px;
+  padding: 40px;
+  background-color: #f5f5f5;
+  border-radius: 6px;
+  display: flex;
+  flex-direction: row;
+}
+.list-list,
+.list-add {
+  text-align: left;
+}
+.list-add {
+  flex-grow: 1;
+}
+.list-list {
+  flex-grow: 2;
+}
+.list {
+  padding: 0;
+}
+.list-item {
+  display: block;
+  padding-bottom: 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
